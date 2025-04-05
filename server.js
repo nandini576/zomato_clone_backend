809533c039819e8eabff95eaec74c9c97af9ffd9
Nandini Kona <nandukona12@gmail.com>
2025-04-05 22:34:15 +0530

    changes made for PORT and URL with home page

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const productRoutes=require('./routes/productRoutes')
 const path=require('path')
 
 //middlware used for sending HTTP request
-const PORT=3000
+const PORT=process.env.PORT || 3000
 const app=express()//assigning express functions to a variable
 app.use(express.json())
 app.use(bodyParser.json())//converts user data into json
